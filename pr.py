40549d5c8b97c3a1fe7dec256a1c1f598a5cf528
Priyamvada Pugalia <priyamvada.pugalia@askinsoftech.com>
2025-08-30 13:06:05 +0530

    update:code

--- pr.py
+++ pr.py
@@ -9,7 +9,7 @@ this will go to master
 
 #code in feature-1 on 10th line for merge conflict checking changing this line again(f-2, next merge keep this)
 
-this is for rebase checking in f-2
+this is for rebase
 
 #hiii
 
